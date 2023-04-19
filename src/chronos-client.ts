abb109e10094c7ae4d5062753b2cf767793c3d71
cc32d9 <cc32d9@gmail.com>
2023-04-19 20:21:47 +0000

    wip

--- src/chronos-client.ts
+++ src/chronos-client.ts
@@ -1,13 +1,12 @@
-import {ABI, ABIDef}  from '@greymass/eosio/src/chain/abi';
-import {abiDecode} from '@greymass/eosio/src/serializer/decoder'
+import {ABI, Serializer}  from '@greymass/eosio';
 
 import cassandra from 'cassandra-driver';
-import shipABIJSON from '../state_history_plugin_abi.json';
+import shipABIJSON from '../state_history_plugin_abi.json' assert {type: 'json'};
 
 export class ChronosClient {
     client: cassandra.Client;
     shipABI: ABI;
-    
+
     constructor(options: cassandra.ClientOptions) {
         this.client = new cassandra.Client(options);
 
@@ -31,20 +30,35 @@ export class ChronosClient {
                                       key_names: elem.key_names,
                                       key_types: [],
                                       type: elem.type});
-            
+
         });
     }
-                                    
+
 
     parseTraceBlob(blob: Buffer): Object {
-        let trace = abiDecode({abi: this.shipABI, data: blob, type: 'transaction_trace'});
+        let trace = Serializer.decode({abi: this.shipABI, data: blob, type: 'transaction_trace'});
         return trace;
     }
 
     async getTraceBlobByTrxID(trx_id: string): Promise<Buffer> {
-        const result = await this.client.execute('SELECT trace FROM transactions where trx_id = ?', [ trx_id ], { prepare : true });
-        const row = result.first();
-        return row.trace;
+        return new Promise<Buffer>((resolve, reject) => {
+            const id_binary = Buffer.from(trx_id, 'hex');
+            if( id_binary.length != 32 ) {
+                reject(new Error('Transaction ID must be 32 bytes encoded as hex: ' + trx_id));
+            }
+
+            this.client.execute('SELECT trace FROM transactions where trx_id = ?',
+                                [ id_binary ],
+                                { prepare : true })
+                .then((result) => {
+                    if( result.rows.length > 0 ) {
+                        resolve(result.rows[0].trace);
+                    }
+                    else {
+                        reject(new Error('Transaction not found: ' + trx_id));
+                    }
+                });
+        });
     }
 }
 
---
+++ src/util/chronos_cli.ts
@@ -0,0 +1,56 @@
+import { Command } from 'commander';
+const program = new Command();
+
+import { ChronosClient } from '../chronos-client.js';
+
+const options = program.opts();
+
+program
+    .requiredOption('--host [value]', 'ScyllaDB host')
+    .requiredOption('--dc [value]', 'ScyllaDB datacenter')
+    .requiredOption('--keyspace [value]', 'Chronos keyspace', 'chronos')
+    .option('--username [value]', 'DB user')
+    .option('--password [value]', 'DB password');
+
+program
+    .command('trxid')
+    .requiredOption('--id [value]', 'tranmsaction ID')
+    .description('retrieve a transaction by ID')
+    .action(async (cmdopts) => {
+
+        const client = get_client();
+        client.getTraceBlobByTrxID(cmdopts.id).then((blob) => {
+            const result = client.parseTraceBlob(blob);
+            console.log(JSON.stringify(result));
+            process.exit(0);
+        });
+    });
+
+
+
+program.parse(process.argv);
+
+
+function get_client() {
+    let conn = { contactPoints: [options.host],
+                 keyspace: options.keyspace,
+                 localDataCenter: options.dc,
+                 credentials: {username: '', password: ''} };
+
+    if( options.username != null ) {
+        if( options.password == null ) {
+            throw Error("password option is required");
+        }
+        conn.credentials = { username: options.username, password: options.username };
+    }
+
+    return new ChronosClient(conn);
+}
+
+
+/*
+ Local Variables:
+ mode: javascript
+ indent-tabs-mode: nil
+ End:
+*/
